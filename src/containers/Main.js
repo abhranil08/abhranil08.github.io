e1e483b01c1e64ee7bb2d95d879e1cb0041fcd5f
Abhranil Bhattacharjee <abhra.tb@gmail.com>
2026-02-13 21:27:03 +0530

    Clean URLs (BrowserRouter + 404), custom domain CNAME
    - Switch HashRouter to BrowserRouter for URLs without #
    - Build: copy index.html to 404.html for GitHub Pages client routing
    - Add public/CNAME (abhranilbhattacharjee.com), remove from .gitignore

--- src/containers/Main.js
+++ src/containers/Main.js
@@ -1,5 +1,5 @@
 import React, { Component } from "react";
-import { Route, Switch, HashRouter } from "react-router-dom";
+import { Route, Switch, BrowserRouter } from "react-router-dom";
 import Home from "../pages/home/HomeComponent";
 import Splash from "../pages/splash/Splash";
 import Education from "../pages/education/EducationComponent";
@@ -19,7 +19,7 @@ export default class Main extends Component {
     if (settings.isSplash) {
       return (
         <div>
-          <HashRouter basename="/">
+          <BrowserRouter basename="/">
             <Switch>
               <Route
                 path="/"
@@ -94,13 +94,13 @@ export default class Main extends Component {
                 )}
               />
             </Switch>
-          </HashRouter>
+          </BrowserRouter>
         </div>
       );
     } else {
       return (
         <div>
-          <HashRouter basename="/">
+          <BrowserRouter basename="/">
             <Switch>
               <Route
                 path="/"
@@ -170,7 +170,7 @@ export default class Main extends Component {
                 )}
               />
             </Switch>
-          </HashRouter>
+          </BrowserRouter>
         </div>
       );
     }
